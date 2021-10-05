c6cd0b2cf96e783351bc3d04aefe65eaa6764339
Heisenberg1111 <30727401+Heisenberg1111@users.noreply.github.com>
2021-10-05 22:36:49 +0800

    added a data logger for temperature measurement

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ myImage.showImage(0, 400)
 basic.pause(500)
 let temp_read = 0
 music.playMelody("D C D F G - B C5 ", 300)
+datalogger.setColumns(["Temperature"])
 basic.forever(function () {
     temp_read += input.temperature()
     basic.pause(500)
@@ -20,8 +21,9 @@ basic.forever(function () {
     } else {
         basic.showIcon(IconNames.Sad)
     }
+    datalogger.logData([datalogger.createCV("Temperature", temp_read)])
     temp_read = 0
-    for (let index = 0; index < 60; index++) {
+    for (let index = 0; index < 120; index++) {
         basic.pause(5000)
     }
 })
